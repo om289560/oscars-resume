cdf66735ee2e36b9ad18c8bc6e483f3c679f81fb
Oscar Miranda <omiranda0058@gmail.com>
2020-01-20 11:00:14 -0600

    did some data adjustments to resumeData.json

--- oscar-resume/server.js
+++ oscar-resume/server.js
@@ -10,16 +10,23 @@ const routes = express();
 //Define Global Variables
 const app = express();
 const log = console.log;
-const PORT = 8080;
+const PORT = process.env.PORT || 8080; // Step 1
 
-mongoose.connect('', {
+// Step 2
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_resume', {
     useNewUrlParser: true
 });
 
 //Configuration
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false});
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use('/', routes);
+
+// Step 3
+
+if (process.env.NODE_ENV === 'production') {
+
+}
 
 app.listen(PORT, () => {
     log(`Server is starting at PORT: ${PORT}`)
