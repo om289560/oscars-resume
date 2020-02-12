5b20896b0a462112b26d111018f5e2569f97ec3c
Oscar Miranda <omiranda0058@gmail.com>
2020-02-12 06:59:25 -0600

    got the porjects to displayas needed along with the images

--- oscar-resume/src/Components/Header.js
+++ oscar-resume/src/Components/Header.js
@@ -34,7 +34,7 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h1 className="responsive-headline">{name}</h1>
             <h3>I'm a Dallas based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
