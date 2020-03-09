e4c38e0913f35f432fff55ce81ea0620e9b12153
Oscar Miranda <omiranda0058@gmail.com>
2020-03-09 10:18:39 -0500

    Updates

---
+++ precache-manifest.0967090341bc5e856cd28e3dc8fcb1a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d380e8b13e39466d3a52ae9ed58f9ae",
+    "url": "/relativepath/index.html"
+  },
+  {
+    "revision": "2d9b84918b550054c271",
+    "url": "/relativepath/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ad07f81051c2b982443d",
+    "url": "/relativepath/static/js/2.7eda1abc.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/relativepath/static/js/2.7eda1abc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d9b84918b550054c271",
+    "url": "/relativepath/static/js/main.cc303c9b.chunk.js"
+  },
+  {
+    "revision": "5c26772348604a44334e",
+    "url": "/relativepath/static/js/runtime-main.8167ceaa.js"
+  }
+]);
